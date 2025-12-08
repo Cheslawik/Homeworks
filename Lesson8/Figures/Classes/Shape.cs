9ed3b63843c8068c250a7d8dbab11c90e2d8bae8
Chaslau Ustsinovich <chustsinovich@gmail.com>
2025-12-08 21:51:35 +0100

    Add task with Figures

---
+++ Lesson8/Figures/Classes/Shape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures.Classes
+{
+    internal abstract class Shape
+    {
+        private double _length;
+        private double _width;
+        private double _height;
+        private double _radius;
+        private double _square;
+        private double _perimeter;
+        private const double _pi = Math.PI;
+        private string[] _recordToArr = new string[5];
+
+
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Radius { get; set; }
+        public double Square { get; set; }
+        public double Perimeter { get; set; }
+
+
+        //public double Length { get { return _length; } set { value = _length; } }
+        //public double Width { get { return _width; } set { value = _width; } }
+        //public double Height { get { return _height; } set { value = _height; } }
+        //public double Radius { get { return _radius; } set { value = _radius; } }
+        //public double Square { get { return _square; } set { value = _square; } }
+        //public double Perimeter { get { return _perimeter; } set { value = _perimeter; } }
+        public double Pi { get { return _pi; } }
+        public string[] RecordToArr { get { return _recordToArr; } set { value = _recordToArr; } }
+
+
+
+        
+        public abstract void GetSquare();
+        public abstract void GetPerimeter();
+
+
+    }
+}
---
+++ Lesson8/Figures/Program.cs
@@ -0,0 +1,63 @@
+﻿using Figures.Classes;
+
+namespace Figures
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            bool isTrue = true;
+            while (isTrue)
+            {
+                Console.WriteLine("\nWelcome to this program!\n");
+
+                Console.WriteLine("1. Rectangle Perimeter");
+                Console.WriteLine("2. Rectangle Square");
+                Console.WriteLine("3. Circle Perimeter");
+                Console.WriteLine("4. Circle Square");
+                Console.WriteLine("5. Triangle Perimeter");
+                Console.WriteLine("6. Triangle Square");
+                Console.WriteLine("\n0. Exit");
+
+                Console.Write("\nChoose the operation: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0":
+                        isTrue = false;
+                        break;
+                    case "1":
+                        Rectangle rectanglePerimeter = new Rectangle();
+                        rectanglePerimeter.GetPerimeter();
+                        break;
+                    case "2":
+                        Rectangle rectangleSquare = new Rectangle();
+                        rectangleSquare.GetSquare();
+                        break;
+                    case "3":
+                        Circle circlePerimeter = new Circle();
+                        circlePerimeter.GetPerimeter();
+                        break;
+                    case "4":
+                        Circle circleSquare = new Circle();
+                        circleSquare.GetSquare();
+                        break;
+                    case "5":
+                        Triangle trianglePerimeter = new Triangle();
+                        trianglePerimeter.GetPerimeter();
+                        break;
+                    case "6":
+                        Triangle triangleSquare = new Triangle();
+                        triangleSquare.GetSquare();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
+
