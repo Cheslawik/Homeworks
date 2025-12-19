97be113f7318f4e24bcea611e6c7ec2abcaf579a
Chaslau Ustsinovich <chustsinovich@gmail.com>
2025-12-19 20:58:15 +0100

    Add homework 10, but don't used delegates, only events.

---
+++ SmartHome/Classes/SecuritySiren.cs
@@ -0,0 +1,45 @@
+﻿using SmartHome.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Classes
+{
+    internal class SecuritySiren : ISmartDevice
+    {
+        public string Name { get; }
+        private bool isOn;
+
+        public SecuritySiren(string name, SmartHomeHub hub)
+        {
+            Name = name;
+            isOn = false;
+            hub.OnEvent += HandleHubEvent;
+        }
+
+        private void HandleHubEvent(object sender, HubEvent e)
+        {
+            ReactToEvent(e);
+        }
+
+        public void ReactToEvent(HubEvent eventData)
+        {
+            if (eventData.Priority > 4)
+            {
+                switch (eventData.EventType)
+                {
+                    case HubEventType.FireAlarm:
+                        isOn = true;
+                        Console.WriteLine($"{Name} turned ON due to fire alarm.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
+
+
---
+++ SmartHome/Classes/SmartLamp.cs
@@ -0,0 +1,45 @@
+﻿using SmartHome.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Classes
+{
+
+    //SmartLamp - подписывается на события хаба в конструкторе
+    //В обработчике события анализирует тип события и его параметры, чтобы решить, включаться или выключаться.
+    internal class SmartLamp : ISmartDevice
+    {
+        public string Name { get; }
+        private bool isOn;
+
+        public SmartLamp(string name, SmartHomeHub hub)
+        {
+            Name = name;
+            isOn = false;
+            hub.OnEvent += HandleHubEvent;
+        }
+
+        private void HandleHubEvent(object sender, HubEvent e)
+        {
+            ReactToEvent(e);
+        }
+        
+        public void ReactToEvent(HubEvent eventData)
+        {
+            switch (eventData.EventType)
+            {
+                case HubEventType.MotionDetected:
+                    isOn = true;
+                    Console.WriteLine($"{Name} turned ON due to motion detected.");
+                    break;
+                case HubEventType.FireAlarm:
+                    isOn = true;
+                    Console.WriteLine($"{Name} turned ON due to fire alarm.");
+                    break;
+            }
+        }
+    }
+}
