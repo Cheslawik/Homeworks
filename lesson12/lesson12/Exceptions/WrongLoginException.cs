eda3f6f834393435b08d1b558527e633b900ff92
Chaslau Ustsinovich <chustsinovich@gmail.com>
2025-12-28 19:43:48 +0100

    Add homework for 12's lesson

---
+++ lesson12/lesson12/Exceptions/WrongLoginException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson12.Exceptions
+{
+    internal class WrongLoginException : Exception
+    {
+        public WrongLoginException() { }
+        public WrongLoginException(string message) : base(message) { }
+    }
+}
---
+++ lesson12/lesson12/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace lesson12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter login, password and confirmation:");
+            Console.Write("Login: ");
+            string _login = Console.ReadLine();
+            Console.Write("Password: ");
+            string _password = Console.ReadLine();
+            Console.Write("Confirm Password: ");
+            string _confirmPassword = Console.ReadLine();
+
+            try
+            {
+                MyClasses.Authentification.Validate(_login, _password, _confirmPassword);
+                Console.WriteLine("You have successfully logged in!");
+            }
+            catch (Exceptions.WrongLoginException ex)
+            {
+                Console.WriteLine($"Login error: {ex.Message}");
+            }
+            catch (Exceptions.WrongPasswordException ex)
+            {
+                Console.WriteLine($"Password error: {ex.Message}");
+            }
+            catch (Exceptions.WrongPasswordConfirmationException ex)
+            {
+                Console.WriteLine($"Password confirmation error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+        }
+    }
+}
